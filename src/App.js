5a2b9fdc6ca1a66a6181ed41d8ba15edf6977e37
Prateek Agrawal <agrawalprateek2807@gmail.com>
2021-01-30 23:52:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1> Todo List App</h1>
+        <form className="todoForm">
+          <input type="text"></input>
+          <button>Go</button>
+        </form>
+
+        <ul>
+          <li>
+          <span>Learn React</span>
+          <button>Edit</button>
+          <button>Delete</button>
+          </li>
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
